2bd7a5e1e49b27829b77282bff7374278c985219
rootfinchreesePOI5 <mohamedabdiaziz515@gmail.com>
2024-11-06 21:14:15 +0300

    forth

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './scss/styles.css'
+import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import ShopContextProvider from './Context/ShopContext.jsx'
 
